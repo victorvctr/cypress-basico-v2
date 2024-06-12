1b0d5b9a96f91eea5df373b956e148ba55aedadb
victo <victorvctr1@hotmail.com>
2024-06-12 02:31:07 -0300

    aula 02

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function() {
         cy.visit('./src/index.html')
-
+    })
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Victor')
+        cy.get('#lastName').type('Rodrigues')
+        cy.get('#email').type('victor@exemplo.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
     })
   })
   
