bd2f29bf0af47ec86748847ba29f7e5de8938b2c
Victor Rodrigues Da Silva <vdsilva_engineering@timbrasil.com.br>
2024-07-05 16:58:46 -0300

    exercicio extra 2: exibe mensagem de erro ao submeter o formulário com um email com formatação inválida

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,14 +8,24 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+    it('preenche os campos obrigatórios e envia o formulário', function() {
         const longText ='Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
         cy.get('#firstName').type('Victor')
         cy.get('#lastName').type('Rodrigues')
         cy.get('#email').type('victor@exemplo.com')
         cy.get('#open-text-area').type(longText, {delay: 0})
         cy.get('button[type="submit"]').click()
+
         cy.get('.success').should('be.visible')
     })
+
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+    cy.get('#firstName').type('Victor')
+        cy.get('#lastName').type('Rodrigues')
+        cy.get('#email').type('victor@exemplo,com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
   })
-  
+})
