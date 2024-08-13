bcef0ab35b097f6114f1b3d4b5f2b29780be9148
victo <victorvctr1@hotmail.com>
2024-08-13 01:19:44 -0300

    aula 6  exercicio extra

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -118,7 +118,7 @@ cy.get('input[type="radio"]')
     .uncheck()
     .should('not.be.checked')
  })
- it.only('seleciona um arquivo da pasta fixtures', function() {
+ it('seleciona um arquivo da pasta fixtures', function() {
     cy.get('input[type="file"]')
     .should('not.have.value')
     .selectFile('./cypress/fixtures/example.json')
@@ -126,5 +126,15 @@ cy.get('input[type="radio"]')
         console.log($input)
         expect($input[0].files[0].name).to.equal('example.json')
     })
+})
+    it('seleciona um arquivo simulando um drag-and-drop', function() {
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+        .should(function($input) {
+            console.log($input)
+            expect($input[0].files[0].name).to.equal('example.json')
+        })   
+    
  })
 })
