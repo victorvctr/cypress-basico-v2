82317385edaecb45252d070472c24240fe80e5fb
victo <victorvctr1@hotmail.com>
2024-08-13 01:36:37 -0300

    aula 6  exercicio extra 2 e inntalação da Extension Cypress Helper

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -135,6 +135,14 @@ cy.get('input[type="radio"]')
             console.log($input)
             expect($input[0].files[0].name).to.equal('example.json')
         })   
-    
+ })
+ it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('input[type="file"]')
+    .selectFile('@sampleFile')
+    .should(function($input) {
+        console.log($input)
+        expect($input[0].files[0].name).to.equal('example.json')
+    })   
  })
 })
