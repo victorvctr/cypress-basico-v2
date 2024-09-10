6029dfe3f927a220796357e71d33c38f75402e73
Victor Rodrigues Da Silva <victor.silva@engdb.com.br>
2024-09-10 17:29:37 -0300

    aula 7 exercicio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -143,6 +143,9 @@ cy.get('input[type="radio"]')
     .should(function($input) {
         console.log($input)
         expect($input[0].files[0].name).to.equal('example.json')
-    })   
- })
+    })  
 })
+   it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+    cy.get('#privacy a').should('have.attr', 'target', '_blank')
+   })
+ })
