4a4ae9e8573ae1c701e3d3bb801f9a80cd44e0f2
Victor Rodrigues Da Silva <victor.silva@engdb.com.br>
2024-09-13 02:02:24 -0300

    Aula 7 Exercício extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -145,7 +145,14 @@ cy.get('input[type="radio"]')
         expect($input[0].files[0].name).to.equal('example.json')
     })  
 })
-   it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
     cy.get('#privacy a').should('have.attr', 'target', '_blank')
+   })
+   it.only('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+    cy.get('#privacy a')
+    .invoke('removeAttr', 'target')
+    .click()
+
+        cy.contains('Talking About Testing').should('be.visible')
    })
  })
